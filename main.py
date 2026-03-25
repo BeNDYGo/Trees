26edddf7ced608481c8ea1a9cb8c5916bd80e292
BeNDYGo <palexascha@gmail.com>
2026-03-25 22:51:46 +0300

    add func

--- main.py
+++ main.py
@@ -1,35 +1,57 @@
 import requests
 import json
 
-url = "https://new.pollen.club/ajax/get_pollen_data"
+# 1 - берёза | 2 - дуб | 3 - ольха | 4 - полынь | 5 - орешник | 6 - злаки | 7 - маревые | 8 - амброзия |
 
-params = {
-    "url": "https://test.pollen.club/maps/ddr_query.php",
-    "method": "indexStat",
-    "params[type]": "5",
-    "params[fromd]": "2026-03-25",
-    "params[fromt]": "0"
-}
+BASE_URL = "https://new.pollen.club/ajax/get_pollen_data"
+CENTER_MOSCOW = (55.7544, 37.6228)
+TOLERANCE = 0.3
 
-response = requests.get(url, params=params)
+def get_pollen_info(alerg_type: int):
+    filters = []
+    all_data_points = []
+    total_val = 0
+    max_val = 0
+    
+    c_lat, c_lon = CENTER_MOSCOW
 
-outer_response = response.json()
-print(outer_response)
+    try:
+        query_params = {
+            "url": "https://test.pollen.club/maps/ddr_query.php",
+            "method": "indexStat",
+            "params[type]": alerg_type, 
+            "params[fromd]": "2026-03-25",
+            "params[fromt]": "0"
+        }
 
-my = "55.7160, 37.6606"
-my_lat, my_lon = map(float, my.split(", "))
-tolerance = 0.1
+        response = requests.get(BASE_URL, params=query_params)
+        data_json = response.json()
 
-if outer_response.get("success"):
-    inner_data = json.loads(outer_response["data"])
+        if data_json.get("success"):
+            inner_data = json.loads(data_json["data"])
 
-    for r in inner_data:
-        date = r["date"]
-        lat, lon = float(r['latitude']), float(r['longitude'])
-        value = r['value']
+            for r in inner_data:
+                date = r["date"]
+                lat, lon = float(r['latitude']), float(r['longitude'])
+                value = int(r['value'])
 
-        print(
-            date[:10],
-            f"({lat, lon})",
-            f"уровень={value}",
-        )
+                all_data_points.append((lat, lon))
+
+                if abs(lat - c_lat) <= TOLERANCE and abs(lon - c_lon) <= TOLERANCE:
+                    filters.append(r)
+                    total_val += value
+                    if value > max_val: max_val = value
+                    '''
+                    print(
+                        date[:10],
+                        f"({lat}, {lon})",
+                        f"уровень={value}",
+                    )
+                    '''
+            print(f"Подошло {len(filters)}/{len(all_data_points)} | среднее значение: {total_val/len(filters):.3f}, max: {max_val}")
+            return filters
+        
+    except Exception as e: print(e)
+    
+for x in range(1, 9):
+    get_pollen_info(x)
